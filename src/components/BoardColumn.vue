96c754f6d4b3e150b0e8df25ab2fe2e39f561975
Hector Vasquez <hvasquezdev@gmail.com>
2020-03-15 19:09:49 -0400

    separating BoardColumn and BoardColumnTask methods in a mixing to avoid repeat code

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -39,18 +39,11 @@
 </template>
 
 <script>
+import movingTasksAndColumns from '@/mixins/movingTasksAndColumns';
+
 export default {
   name: 'board-column',
-  props: {
-    column: {
-      type: Object,
-      required: true
-    },
-    board: {
-      type: Object,
-      required: true
-    }
-  },
+  mixins: [movingTasksAndColumns],
   components: {
     BoardColumnTask: () => import('@/components/BoardColumnTask')
   },
@@ -72,41 +65,6 @@ export default {
 
       e.dataTransfer.setData('from-column-index', columnIndex);
       e.dataTransfer.setData('type', 'column');
-    },
-    moveTaskOrColumn(e, toTasks, toColumn, toTask) {
-      const type = e.dataTransfer.getData('type');
-
-      switch (type) {
-        case 'task':
-          this.moveTask(e, toTasks, toTask);
-          break;
-
-        default:
-          this.moveColumn(e, toColumn);
-          break;
-      }
-    },
-    moveTask(e, toTasks, toTask = undefined) {
-      const fromIndex = e.dataTransfer.getData('from-column-index');
-      const taskIndex = e.dataTransfer.getData('task-index');
-      const fromTasks = this.board.columns[fromIndex].tasks;
-      const toTaskIndex = toTasks.indexOf(toTask);
-
-      this.$store.commit('MOVE_TASK', {
-        from: fromTasks,
-        fromIndex: taskIndex,
-        to: toTasks,
-        toIndex: toTaskIndex
-      });
-    },
-    moveColumn(e, toColumn) {
-      const fromIndex = e.dataTransfer.getData('from-column-index');
-      const toIndex = this.board.columns.indexOf(toColumn);
-
-      this.$store.commit('MOVE_COLUMN', {
-        fromIndex,
-        toIndex
-      });
     },
     updateColumnName(e, column) {
       if (!e.target.value.trim() || !column) return;
--- src/components/BoardColumnTask.vue
+++ src/components/BoardColumnTask.vue
@@ -20,18 +20,13 @@
 </template>
 
 <script>
+import movingTasksAndColumns from '@/mixins/movingTasksAndColumns';
+
 export default {
   name: 'board-column-task',
+  mixins: [movingTasksAndColumns],
   props: {
     task: {
-      type: Object,
-      required: true
-    },
-    column: {
-      type: Object,
-      required: true
-    },
-    board: {
       type: Object,
       required: true
     }
@@ -57,32 +52,6 @@ export default {
       e.dataTransfer.setData('task-index', taskIndex);
       e.dataTransfer.setData('from-column-index', columnIndex);
       e.dataTransfer.setData('type', 'task');
-    },
-    moveTaskOrColumn(e, toTasks, toColumn, toTask) {
-      const type = e.dataTransfer.getData('type');
-
-      switch (type) {
-        case 'task':
-          this.moveTask(e, toTasks, toTask);
-          break;
-
-        default:
-          this.moveColumn(e, toColumn);
-          break;
-      }
-    },
-    moveTask(e, toTasks, toTask = undefined) {
-      const fromIndex = e.dataTransfer.getData('from-column-index');
-      const taskIndex = e.dataTransfer.getData('task-index');
-      const fromTasks = this.board.columns[fromIndex].tasks;
-      const toTaskIndex = toTasks.indexOf(toTask);
-
-      this.$store.commit('MOVE_TASK', {
-        from: fromTasks,
-        fromIndex: taskIndex,
-        to: toTasks,
-        toIndex: toTaskIndex
-      });
     }
   }
 };
